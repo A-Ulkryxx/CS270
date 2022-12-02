cba816dec2b3466a7966f3cc6448fb3d607c1f2a
Ulkryxx <ayoungren94@gmail.com>
2022-12-02 13:38:46 -0700

    Almighy Push

---
+++ StackDump/pointer.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+        //PART1
+    int i;  	// an integer value
+    int *pi;	// a pointer to an integer value
+    
+    i = -1;
+    pi = &i ;	// set pi to the address of i
+    
+    printf("i: %d, pi: %p > *pi: %d\n", i, pi, *pi);
+    
+    // place additional code here
+        //PART2
+    int **ppi;
+    ppi = &pi ; // set ppi to the address of pi
+    
+    printf("i: %d, pi: %p > *pi: %d, ppi: %p > **ppi: %d \n", i, pi, *pi, ppi, **ppi);
+    
+        //PART3
+    int memory[] = {1,2,3,4,5,6,7,8,9};
+    int *pm;
+
+    pm = memory;  // &memory[0] is an alternative.
+    for (int i=0; i<9; i++) {
+        printf("pm: %p > *pm: %d\n", pm, *pm);
+        pm++;
+    }
+
+        //PART4
+    int *nm;
+    pm = memory;
+    nm = (int *) *pm;
+    printf ("pm: %p, nm: %p\n", pm, nm);
+
+    return 0;    
+} 
